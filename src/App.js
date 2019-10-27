2ba2a8ab7a58680d31d7f1166640292585b65e53
sankincn <tfx919@163.com>
2019-10-27 08:39:55 +0800

    update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is a React project.
+          React CI.
         </p>
         <a
           className="App-link"
