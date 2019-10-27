cba25a46989bfd002191b3b6c1c3c931d74b8da1
sankincn <tfx919@163.com>
2019-10-27 09:20:48 +0800

    Updates

---
+++ precache-manifest.fcd561f243969dcc123e59e6f235e872.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03ff84e385905ef7db42aef581b87578",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ae0934d58422270e6c9",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "b922911e1dec3e2d52aa",
+    "url": "/static/js/2.38db2f4a.chunk.js"
+  },
+  {
+    "revision": "1ae0934d58422270e6c9",
+    "url": "/static/js/main.41bf250d.chunk.js"
+  },
+  {
+    "revision": "55179e28a5170944f793",
+    "url": "/static/js/runtime-main.fdcdd08d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
